eaa3156a3ca94e2d4a61e0280e7b5b1cf3aba994
tomato464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2021-02-17 21:05:52 +0900

    timer apiのbugを修正。TIMER にflags2を追加。

--- hariboteMain/timer.c
+++ hariboteMain/timer.c
@@ -36,6 +36,7 @@ struct TIMER *timer_alloc(void)
 	for (i = 0; i < MAX_TIMER; i++) {
 		if (timerctl.timers0[i].flags == 0) {
 			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			timerctl.timers0[i].flags2 = 0;
 			return &timerctl.timers0[i];
 		}
 	}
@@ -115,5 +116,53 @@ void inthandler20(int *esp)
 	if (ts != 0) {
 		task_switch();
 	}
+	return;
+}
+
+int timer_cancel(struct TIMER *timer)
+{
+	int e;
+	struct TIMER *t;
+
+	e = io_load_eflags();
+	io_cli();//キャンセル中にタイマの状態が変わると困る
+
+	if(timer->flags == TIMER_FLAGS_USING){//キャンセルが必要かどうかを調べる
+		if(t == timerctl.t0){//タイマが先頭だった場合
+			t = timer->next;
+			timerctl.t0 = t;
+			timerctl.next = t->timeout;
+		}else{//先頭でないとき
+			t = &timerctl.t0;
+			for(;;){//timerの一つ前のものを探す
+				if(t->next == timer){
+					break;
+				}
+				t = t->next;
+			}
+			t->next = timer->next;
+		}
+		timer->flags = TIMER_FLAGS_ALLOC;
+		io_store_eflags(e);
+		return 1;//キャンセル処理成功
+	}
+	io_store_eflags(e);
+	return 0;//キャンセル処理は失敗
+}
+
+void timer_cancelall(struct FIFO32 *fifo)
+{
+	int i,e;
+	struct TIMER *t;
+	e = io_load_eflags();
+	io_cli();
+	for(i = 0; i < MAX_TIMER; i++){
+		t = &timerctl.timers0[i];
+		if(t->flags != 0 && t->flags2 != 0 && t->fifo == fifo){
+			timer_cancel(t);
+			timer_free(t);
+		}
+	}
+	io_store_eflags(e);
 	return;
 }
